ca0ebbc571ca3ddd432248865f5b0e6a0f38ac09
Allenboy <32590722+haidragon@users.noreply.github.com>
2019-04-25 15:15:24 +0800

    Add files via upload

---
+++ inlinehook1/j111111/obj/local/armeabi/objs/hook/inlineHook.o.d
@@ -0,0 +1,188 @@
+/Users/haidragon/Desktop/androidteset/obj/local/armeabi/objs/hook/inlineHook.o: \
+ /Users/haidragon/Desktop/androidteset/jni/inlineHook.c \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/dirent.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/signal.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ucontext.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/user.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/mman.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/mman.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/mman.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/mman-common.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/wait.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/resource.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/resource.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/resource.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/resource.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/wait.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/unistd.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syscall.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/errno.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/errno.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/errno.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ptrace.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/ptrace.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/ptrace.h \
+ /Users/haidragon/Desktop/androidteset/jni/relocate.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /Users/haidragon/Desktop/androidteset/jni/include/inlineHook.h
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/string.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/dirent.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ucontext.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/user.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/sigcontext.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/mman.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/mman.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/mman.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/mman-common.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/wait.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/resource.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/resource.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/resource.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/resource.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/wait.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/unistd.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/syscall.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/errno.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/errno.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/errno.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/ptrace.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/ptrace.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/ptrace.h:
+
+/Users/haidragon/Desktop/androidteset/jni/relocate.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/Users/haidragon/Desktop/androidteset/jni/include/inlineHook.h:
---
+++ inlinehook1/obj/local/armeabi/objs/hook/relocate.o.d
@@ -0,0 +1,56 @@
+/Users/haidragon/Desktop/androidteset/obj/local/armeabi/objs/hook/relocate.o: \
+ /Users/haidragon/Desktop/androidteset/jni/relocate.c \
+ /Users/haidragon/Desktop/androidteset/jni/relocate.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h
+
+/Users/haidragon/Desktop/androidteset/jni/relocate.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Users/haidragon/Library/Android/sdk/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
